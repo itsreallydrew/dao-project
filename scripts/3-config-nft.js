bf4fb6a6e7e1827cecca5949f500c9c569b1df33
Andrew McLean <88289750+itsreallydrew@users.noreply.github.com>
2021-12-20 19:52:30 +0000

    configured membership nft

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -2,16 +2,16 @@ import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
 const bundleDrop = sdk.getBundleDropModule(
-  "0x5B05a512cCA0F4A40462c5A6D8D1d1dc3402B17CS",
+  "0x5B05a512cCA0F4A40462c5A6D8D1d1dc3402B17C",
 );
 
 (async () => {
   try {
     await bundleDrop.createBatch([
       {
-        name: "Leaf Village Headband",
-        description: "This NFT will give you access to NarutoDAO!",
-        image: readFileSync("scripts/assets/headband.png"),
+        name: "OG 1's",
+        description: "This NFT will give you access to SNKRDAO!",
+        image: readFileSync("scripts/assets/air-jordan-1-retro-og-hero_xf8joi.jpg"),
       },
     ]);
     console.log("✅ Successfully created a new NFT in the drop!");
